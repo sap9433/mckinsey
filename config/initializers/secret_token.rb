7a6afe8c78f609564519aca06049a2ab6d301839
unknown <Saptarshi Chatterjee@GGN000111137002.ads.mckinsey.com>
2014-02-23 16:29:57 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mckinsey::Application.config.secret_key_base = '54ba48386cbbdfdbebe04f6a6675c59b0e75ad7a775b1e1a2105d05e4d3e9166e53feead5bdabc9977c124f4b212db4924f5492a5b0a9b7ca66b89305e0e026a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Mckinsey::Application.config.session_store :cookie_store, key: '_mckinsey_session'
