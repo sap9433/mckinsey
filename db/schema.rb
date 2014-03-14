f2d2c4745367c5c7c87241c3a5c5484fc9a7aaa0
Saptarshi Chatterjee <saptarshichatterjee1@gmail.com>
2014-03-15 00:51:35 +0530

    initial commit sap

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20140223113925) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "meetings", force: true do |t|
     t.string   "uid"
     t.string   "name"
