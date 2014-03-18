a0253c76eebe2a0ce19b1ae2328c7a32797e192e
Saptarshi Chatterjee <saptarshichatterjee1@gmail.com>
2014-03-18 21:19:57 +0530

    filepicker initial working

--- app/assets/javascripts/meeting.js
+++ app/assets/javascripts/meeting.js
@@ -20,4 +20,21 @@ angular.module('meetingApp', ['ngRoute', 'ngAnimate'])
     {name:'Robert De Niro'},
     {name:'Christian Bale'},
     {name:'mark zuckerberg'}];
+
+  $scope.uploadToCloud = function() {
+   filepicker.setKey('A2kM2lyAMQqK2DgFwwJvAz');
+   filepicker.pick({
+      mimetypes: ['image/*', 'text/plain'],
+      container: 'window',
+      services:['COMPUTER', 'FACEBOOK', 'GMAIL'],
+    },
+    function(InkBlob){
+      console.log(JSON.stringify(InkBlob));
+    },
+    function(FPError){
+      console.log(FPError.toString());
+    }
+   );
+  }
+
 }]);
